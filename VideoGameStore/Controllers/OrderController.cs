6b45eab05ad3850763c6e34ec349b156173c3cbb
Kupru3 <unecr.one@gmail.com>
2025-12-07 21:00:54 +0300

    create controllers

---
+++ VideoGameStore/Controllers/OrderController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VideoGameStore.Dtos;
+using VideoGameStore.Services;
+
+namespace VideoGameStore.Controllers
+{
+    [Route("api/orders")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IOrderService _orderService;
+
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        [Route("api/orders/")]
+        [HttpGet]
+        public IActionResult GetPageOrders([FromQuery]Pageable pageable)
+        {
+            
+        }
+
+    }
+}
